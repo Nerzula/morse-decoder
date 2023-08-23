4967bc705f26cf4c227af33c269464472911a8e5
Alex <alexzloid@gmail.com>
2023-08-23 12:41:05 +0500

    feat: create task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+	'.-': 'a',
+	'-...': 'b',
+	'-.-.': 'c',
+	'-..': 'd',
+	'.': 'e',
+	'..-.': 'f',
+	'--.': 'g',
+	'....': 'h',
+	'..': 'i',
+	'.---': 'j',
+	'-.-': 'k',
+	'.-..': 'l',
+	'--': 'm',
+	'-.': 'n',
+	'---': 'o',
+	'.--.': 'p',
+	'--.-': 'q',
+	'.-.': 'r',
+	'...': 's',
+	'-': 't',
+	'..-': 'u',
+	'...-': 'v',
+	'.--': 'w',
+	'-..-': 'x',
+	'-.--': 'y',
+	'--..': 'z',
+	'.----': '1',
+	'..---': '2',
+	'...--': '3',
+	'....-': '4',
+	'.....': '5',
+	'-....': '6',
+	'--...': '7',
+	'---..': '8',
+	'----.': '9',
+	'-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+	const morse = getMorse(expr);
+
+	function getMorse(str) {
+		const getChanks = splitStringIntoChunks(str, 10);
+		const res = getChanks.map(chank => {
+			if (chank == '**********') {
+				return ' ';
+			}
+			const morse = splitStringIntoChunks(chank, 2).reduce((acc, val) => {
+				if (val == 10) {
+					acc += '.';
+				} else if (val == 11) {
+					acc += '-';
+				}
+				return acc;
+			}, '');
+			return morse;
+		});
+		return res;
+	}
+	function splitStringIntoChunks(string, chunkSize) {
+		const regex = new RegExp(`.{1,${chunkSize}}`, 'g');
+		return string.match(regex);
+	}
+
+	return morse.map(a => MORSE_TABLE[a] || ' ').join('');
 }
 
 module.exports = {
-    decode
-}
+	decode,
+};
